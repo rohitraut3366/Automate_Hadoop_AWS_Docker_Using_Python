5ffee7a096ba85a156be57e903b323c12ee2b933
rohitraut3366 <rohitraut3366@gmail.com>
2020-11-08 21:54:18 +0530

    submit

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ from AWS.aws import Aws
 from BIGDATA.hadoop import HadoopMainMenu
 from Devops.Docker.Docker import dockerMain
 from Linux.LVM import LVM
+from Linux.webserver import webserverMain
 
 menuPass = "1"
 x = getpass.getpass()
@@ -20,8 +21,9 @@ def mainMenu():
                     Press 1: For Hadoop
                     Press 2: For AWS	
                     Press 3: For Lvm
-                    Press 4: For Docker 
-                    Press 5: For Exit
+                    Press 4: For Docker
+                    Press 5: For Webserver 
+                    Press 6: For Exit
              """)
         os.system('tput setaf 7')
         choice = input("\n Enter Your Choice:")
@@ -33,6 +35,8 @@ def mainMenu():
             LVM()
         elif choice == '4':
             dockerMain()
+        elif choice == "5":
+            webserverMain()
         else:
             exit()
 
